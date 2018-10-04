376169e31c9f4e2799dc6332ac04d65defe27c5c
kkterai <kkterai@gmail.com>
2018-10-04 15:54:54 -0700

    Add event listener to input form

--- src/Input.js
+++ src/Input.js
@@ -4,15 +4,25 @@ class Input extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            githubUsername: ""
+            githubUsername: "",
+            name: "", 
+            email: "", 
+            company: "", 
+            public_repos: ""
         }
     }
+
+    handleUsernameChange = event => {
+        this.setState({
+          githubUsername: event.target.value
+        })
+      }
   
     render() {
       return (
         <form>
             <h2>Enter a Github username:</h2>
-            <input type="text" name="githubUsername" value={this.state.githubUsername} />
+            <input type="text" name="githubUsername" onChange={event => this.handleUsernameChange(event)} value={this.state.githubUsername} />
         </form>
       )
     }
