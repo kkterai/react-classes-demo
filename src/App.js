c84c3adebf004a4d793628eff05b8930837ec8e5
kkterai <kkterai@gmail.com>
2018-10-04 22:32:07 -0700

    Add fetch to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 
-import Input from './Input'
+//import Data from './Data'
 
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        githubUsername: "",
+        data: null,
+    };
+}
+
+componentDidMount() {
+  fetch('https://api.github.com/users')
+    .then(response => response.json())
+    .then(data => this.setState({ data }));
+}
+
+handleSubmit(event) {
+  event.preventDefault();
+  debugger
+  this.sendFormDataSomewhere(event)
+  this.setState({ githubUsername: "" })
+}
+
+searchUsers(username) {
+  this.state.data.filter(object => object.login === username)
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Input/>
+           <form onSubmit={event => this.handleSubmit(event)}>
+              <h2>Enter a Github username:</h2>
+              <input type="text" name="githubUsername" onChange={event => this.searchUsers(event)} value={this.state.githubUsername} />
+          </form>
         </header>
-       
-
       </div>
     );
   }
---
+++ src/Data.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Data = ({name}) => (
+ <div>{`Hi ${name}`}</div>
+);
+
+export default Data;
